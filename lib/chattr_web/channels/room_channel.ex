e32351a3dc014056fd2919bfc98ec4ed76d37838
nicholedlc <nichole@spark.re>
2017-12-19 18:35:38 -0800

    Handle incoming events

--- lib/chattr_web/channels/room_channel.ex
+++ lib/chattr_web/channels/room_channel.ex
@@ -8,4 +8,9 @@ defmodule ChattrWeb.RoomChannel do
   def join("room:" <> _private_room_id, _params, _socket) do
     {:error, %{reason: "unauthorized"}}
   end
+
+  def handle_in("new_msg", %{"body" => body}, socket) do
+    broadcast! socket, "new_msg", %{body: body}
+    {:noreply, socket}
+  end
 end
